ff346793f065dc6a0453ce1b3e23906cae5bb9cb
Fombi Favour <fombifavour@gmail.com>
2022-07-24 22:41:33 +0200

    added components

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Navbar from '../components/Navbar'
+import TitleBar from '../components/Titlebar'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <TitleBar />
     </div>
   )
 }
