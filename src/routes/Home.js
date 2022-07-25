9063587983e9a7adaff686fbbba543ba324b0fc1
Fombi Favour <fombifavour@gmail.com>
2022-07-25 12:26:24 +0200

    editting commit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 import Navbar from '../components/Navbar'
 import TitleBar from '../components/Titlebar'
+import Image from '../components/Image'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <TitleBar />
+        <Image />
     </div>
   )
 }
