c76ed916d0e2d6ccccf1c2522b2b05c520dc553b
Fombi-Favour <fombifavour@gmail.com>
2022-08-13 00:32:59 +0100

    edit titlebar components

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 import Navbar from '../components/Navbar'
 import TitleBar from '../components/Titlebar'
-import Image from '../components/Image'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
         <TitleBar />
-        <Image />
     </div>
   )
 }
