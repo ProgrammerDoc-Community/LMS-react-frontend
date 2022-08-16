96c83404523c845199387ce250fcba08d946090d
Fombi-Favour <fombifavour@gmail.com>
2022-08-16 23:47:55 +0100

    edit card component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,7 +6,7 @@ import Upload from '../assets/upload.png'
 
 const Cards = () => {
   return (
-    <div className='card-container '>
+    <>
       <div className='card1'>
         <img src={Session} alt='Session' />
         <span className='content'>Efficient Sessions</span>
@@ -14,9 +14,9 @@ const Cards = () => {
       </div>
 
       <div className='card2'>
-        <img src={Course} alt='Course' />
         <span className='content'>Courses and Subject</span>
         <p className='spec'>Students able to select a large number of courses and subject</p>
+        <img src={Course} alt='Course' />
       </div>
 
       <div className='card3'>
@@ -24,7 +24,7 @@ const Cards = () => {
         <span className='content'>Upload Resources</span> <br></br>
         <p className='spec'>Teachers able to upload notes and other materials for the students</p>
       </div>
-    </div>
+    </>
   )
 }
 
