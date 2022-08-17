ac7579303808917b22bbc37072a9c3d6972860d8
Fombi-Favour <fombifavour@gmail.com>
2022-08-17 01:06:56 +0100

    update cards component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,21 +8,21 @@ const Cards = () => {
   return (
     <>
       <div className='card1'>
-        <img src={Session} alt='Session' />
+         <img src={Session} alt='Session' />
         <span className='content'>Efficient Sessions</span>
-        <p className='spec'>Create video, audio and chat sessions</p>
+        <p className='spec1'>Create video, audio and chat sessions</p>
       </div>
 
       <div className='card2'>
         <span className='content'>Courses and Subject</span>
-        <p className='spec'>Students able to select a large number of courses and subject</p>
+        <p className='spec2'>Students able to select a large number of courses and subject</p>
         <img src={Course} alt='Course' />
       </div>
 
       <div className='card3'>
         <img src={Upload} alt='Upload' />
         <span className='content'>Upload Resources</span> <br></br>
-        <p className='spec'>Teachers able to upload notes and other materials for the students</p>
+        <p className='spec3'>Teachers able to upload notes and other materials for the students</p>
       </div>
     </>
   )
