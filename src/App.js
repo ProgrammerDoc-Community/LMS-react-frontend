a0b479655a96d09e718f8280a1b305dead43139a
Fombi-Favour <fombifavour@gmail.com>
2022-10-20 04:48:11 +0100

    refresh and add dependencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import React from 'react'
-import {Routes, Route} from 'react-router-dom';
-
-import Home from './routes/Home';
-import Register from './routes/Register';
-import Student from './routes/Student';
-import Teacher from './routes/Teacher';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from './Routes/Home'
 
 const App = () => {
-  
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/student' element={<Student />} />
-        <Route path='/teacher' element={<Teacher />} />
       </Routes>
     </>
   );
-}
+};
 
-export default App
+export default App;
